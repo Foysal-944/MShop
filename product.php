fbdf7d95d60004f49ad7cfce67d1cb6298eb1213
Foysal-944 <70658012+Foysal-944@users.noreply.github.com>
2020-09-02 23:55:18 +0600

    Add files via upload

---
+++ product.php
@@ -0,0 +1,24 @@
+<?php
+    
+    // include header.php file
+    include ('header.php');
+?>
+
+<?php
+
+include ('Template/_products.php');
+  
+
+    /*  include top sale section */
+        include ('Template/_top-sale.php');
+    /*  include top sale section */
+
+   
+
+?>
+
+
+<?php
+// include footer.php file
+include ('footer.php');
+?>
